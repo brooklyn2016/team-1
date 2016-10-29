1d35d01ec70f09b6b626c95e5da7fad60318539f
nhamlani101 <nh1083@nyu.edu>
2016-10-28 22:00:31 -0400

    playback tests working on web

--- Web/app.js
+++ Web/app.js
@@ -1,28 +1,18 @@
 (function localFileVideoPlayer() {
 	'use strict'
-  var URL = window.URL || window.webkitURL
-  var displayMessage = function (message, isError) {
-    var element = document.querySelector('#message')
-    element.innerHTML = message
-    element.className = isError ? 'error' : 'info'
-  }
-  var playSelectedFile = function (event) {
-    var file = this.files[0]
-    var type = file.type
-    var videoNode = document.querySelector('video')
-    var canPlay = videoNode.canPlayType(type)
-    if (canPlay === '') canPlay = 'no'
-    var message = 'Can play type "' + type + '": ' + canPlay
-    var isError = canPlay === 'no'
-    displayMessage(message, isError)
+  var videoSources = new Array();
+  videoSources[0] = 'videos/1.mp4';
+  videoSources[1] = 'videos/2.mp4';
+    
+    document.getElementById("myVideo").setAttribute("src", videoSources[0])
 
-    if (isError) {
-      return
-    }
-
-    var fileURL = URL.createObjectURL(file)
-    videoNode.src = fileURL
-  }
-  var inputNode = document.querySelector('input')
-  inputNode.addEventListener('change', playSelectedFile, false)
-})()
+function videoPlay() {
+    document.getElementById("myVideo").load();
+    document.getElementById("myVideo").play();
+    document.getElementById("myVideo").setAttribute("src", videoSources[1]);
+    document.getElementById("myVideo").load();
+    document.getElementById("myVideo").play(); 
+}
+}
+ 
+)()
